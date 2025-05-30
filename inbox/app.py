092d8e55667d5b55b0e881b17d83805e4b37ee0a
tifa <hello@tifa.dev>
2025-05-30 06:43:05 -0700

    Google Analytics

--- inbox/app.py
+++ inbox/app.py
@@ -12,6 +12,17 @@ from inbox.service.auth import AuthMiddleware
 initialize_db()
 app.add_middleware(AuthMiddleware)
 
+GOOGLE_ANALYTICS_ID = os.environ.get("GOOGLE_ANALYTICS_ID")
+if GOOGLE_ANALYTICS_ID:
+    ui.add_head_html(f"""
+    <script async src="https://www.googletagmanager.com/gtag/js?id={GOOGLE_ANALYTICS_ID}"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){{dataLayer.push(arguments);}}
+        gtag('js', new Date());
+        gtag('config', '{GOOGLE_ANALYTICS_ID}');
+    </script>
+    """)
 
 if __name__ in {"__main__", "__mp_main__"}:
     storage_secret = secrets.token_hex(32)
